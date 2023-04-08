ea18f0ee67bd921f0254ee5b4760c6e6718e9d18
Christophe <christophe.lombart@gmail.com>
2023-04-08 10:11:54 +0200

    Add new completion parameters

--- src/post.ts
+++ src/post.ts
@@ -1,7 +1,7 @@
 import { oraPromise } from 'ora'
 import { ChatGPTAPI, ChatMessage } from 'chatgpt'
 import pRetry, { AbortError } from 'p-retry'
-import { extractPostOutlineFromCodeBlock, extractHtmlCodeBlock } from './lib/extractor'
+import { extractPostOutlineFromCodeBlock, extractHtmlCodeBlock, extractJsonArray } from './lib/extractor'
 import {
   getPromptForOutline,
   getPromptForIntroduction,
@@ -10,14 +10,26 @@ import {
   getPromptForSeoTitle,
   getPromptForSeoDescription,
   getPromptForUrl,
-  getPromptForWritingLikeAHuman
+  getPromptForWritingLikeAHuman,
+  getPromptForMainKeyword
 } from './lib/prompts'
-import { PostPrompt, Section, PostOutline, PostGeneratorOptions, Post } from './types'
+import {
+  PostPrompt,
+  Section,
+  PostOutline,
+  PostGeneratorOptions,
+  Post,
+  CompletionParams
+} from './types'
+
+import { encode } from './lib/tokenizer'
 
 export interface GeneratorHelperInterface {
+  init () : Promise<void>
   getPrompt () : PostPrompt
-  generateContentOutline () : Promise<PostOutline>
   askToWriteLikeAHuman () : Promise<String>
+  generateContentOutline () : Promise<PostOutline>
+  generateMainKeyword () : Promise<string[]>
   generateIntroduction () : Promise<string>
   generateConclusion () : Promise<string>
   generateSectionContents (tableOfContent : PostOutline) : Promise<string>
@@ -29,11 +41,15 @@ export interface GeneratorHelperInterface {
 export class ChatGptHelper implements GeneratorHelperInterface {
   private api : ChatGPTAPI
   private chatMessage : ChatMessage
+  private completionParams : CompletionParams
 
   public constructor (private postPrompt : PostPrompt, private options? : PostGeneratorOptions) {
     this.api = new ChatGPTAPI({
       apiKey: options?.apiKey || process.env.OPENAI_API_KEY,
-      completionParams: { model: options?.model || 'gpt-4' },
+      completionParams: {
+        model: options?.model || 'gpt-4'
+      },
+
       maxModelTokens: options?.maxModelTokens || 8100 // not 8192 because we're leaving some buffer room
     })
 
@@ -42,14 +58,57 @@ export class ChatGptHelper implements GeneratorHelperInterface {
     }
   }
 
+  async init () {
+    const mainKwWords = await this.generateMainKeyword()
+
+    // set the logit bias in order to force the model to minimize the usage of the main keyword
+    const logitBiais : Record<number, number> = {}
+    mainKwWords.forEach((kw) => {
+      const encoded = encode(kw)
+      encoded.forEach((element) => {
+        logitBiais[element] = Number(this.postPrompt?.logitBias) || -1
+      })
+    })
+
+    this.completionParams = {
+      temperature: this.postPrompt?.temperature || 0.7,
+      frequency_penalty: this.postPrompt?.frequencyPenalty || -0.5,
+      presence_penalty: this.postPrompt?.presencePenalty || 0.5,
+      logit_bias: logitBiais
+    }
+
+    if (this.options?.debug) {
+      console.log('---------- COMPLETION PARAMETERS ----------')
+      console.log('Temperature : ' + this.completionParams.temperature)
+      console.log('Frequency Penalty : ' + this.completionParams.frequency_penalty)
+      console.log('Presence Penalty' + this.completionParams.presence_penalty)
+      console.log('Logit Biais' + this.completionParams.logit_bias)
+    }
+  }
+
   public getPrompt () {
     return this.postPrompt
+  }
+
+  async generateMainKeyword () {
+    const prompt = getPromptForMainKeyword(this.postPrompt)
+    if (this.options?.debug) {
+      console.log('---------- PROMPT MAIN KEYWORD ----------')
+      console.log(prompt)
+    }
+    this.chatMessage = await this.sendRequest(prompt)
+    if (this.options?.debug) {
+      console.log('---------- OUTLINE ----------')
+      console.log(this.chatMessage.text)
+    }
+
+    return extractJsonArray(this.chatMessage.text)
   }
 
   async generateContentOutline () {
     const prompt = getPromptForOutline(this.postPrompt)
     if (this.options?.debug) {
-      console.log('---------- PROMPT ----------')
+      console.log('---------- PROMPT OUTLINE ----------')
       console.log(prompt)
     }
     this.chatMessage = await this.sendRequest(prompt)
@@ -71,12 +130,12 @@ export class ChatGptHelper implements GeneratorHelperInterface {
   }
 
   async generateIntroduction () {
-    this.chatMessage = await this.sendRequest(getPromptForIntroduction(this.postPrompt.language))
+    this.chatMessage = await this.sendRequest(getPromptForIntroduction(this.postPrompt.language), this.completionParams)
     return extractHtmlCodeBlock(this.chatMessage.text)
   }
 
   async generateConclusion () {
-    this.chatMessage = await this.sendRequest(getPromptForConclusion(this.postPrompt.language))
+    this.chatMessage = await this.sendRequest(getPromptForConclusion(this.postPrompt.language), this.completionParams)
     return extractHtmlCodeBlock(this.chatMessage.text)
   }
 
@@ -124,18 +183,23 @@ export class ChatGptHelper implements GeneratorHelperInterface {
     if (this.options?.debug) {
       console.log(`\nChapter : ${chapterDescription}  ...'\n`)
     }
-    this.chatMessage = await this.sendRequest(getPromptForSection(language, chapterDescription, keywords))
+    this.chatMessage = await this.sendRequest(getPromptForSection(language, chapterDescription, keywords), this.completionParams)
     return `${extractHtmlCodeBlock(this.chatMessage.text)}\n\n`
   }
 
-  private async sendRequest (prompt : string) {
+  private async sendRequest (prompt : string, completionParams? : CompletionParams) {
     return await pRetry(async () => {
       return await this.api.sendMessage(prompt, {
-        parentMessageId: this.chatMessage?.id
+        parentMessageId: this.chatMessage?.id,
+        completionParams
       })
     }, {
       retries: 10,
       onFailedAttempt: async (error) => {
+        if (this.options?.debug) {
+          console.log('---------- OPENAI REQUEST ERROR ----------')
+          console.log(error)
+        }
         if (error instanceof AbortError) {
           console.log('OpenAI API - Request aborted')
         } else {
@@ -153,6 +217,13 @@ export class PostGenerator {
   }
 
   public async generate () : Promise<Post> {
+    await oraPromise(
+      this.helper.init(),
+      {
+        text: ' Init the post ...'
+      }
+    )
+
     await oraPromise(
       this.helper.askToWriteLikeAHuman(),
       {
@@ -215,7 +286,7 @@ export class PostGenerator {
       slug,
       seoTitle,
       seoDescription,
-      title : tableOfContent.title,
+      title: tableOfContent.title,
       content: `${introduction}\n${htmlContent}`
     }
   }
