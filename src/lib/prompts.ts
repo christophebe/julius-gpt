f5c13293b2e86399ab09702c754323b388f46dfb
Christophe <christophe.lombart@gmail.com>
2023-04-29 10:05:38 +0200

    Review some prompts

--- src/lib/prompts.ts
+++ src/lib/prompts.ts
@@ -7,8 +7,8 @@ const STRUCTURE_OUTLINE = 'Generate the blog post outline with the following jso
   '"headings": [ // If necessary, add subheadings here. This is optional' +
     '{ "title": "", ' + '"keywords": ["...", "..."] },' +
     '{ "title": "", "keywords": ["...", "..."] }, ... ] } ... ],' +
-'"slug" : "", // Use the main keywords for the slug based on the topic of the post. Max 3 or 4 keywords' +
-'"seoTitle" : "", // not the same as the post tile, max 60 characters ' +
+'"slug" : "", // Use the main keywords for the slug based on the topic of the post. Do not mention the country.  Max 3 or 4 keywords, without stop words and with text normalization and accent stripping' +
+'"seoTitle" : "", // not the same as the post tile, max 60 characters, do not mention the country' +
 '"seoDescription : "" //max 155 characters }'
 
 export function getSystemPrompt (post : PostPrompt) {
@@ -19,7 +19,7 @@ export function getPromptForOutline (postPrompt : PostPrompt) {
   const { country, intent, audience } = postPrompt
   const prompt = STRUCTURE_OUTLINE +
     'Do not add heading for an introduction, conclusion or to summarize the article.' +
-    (country === 'none' ? '' : 'Market/country/region :' + country + '.') +
+    (country === null || country === 'none' ? '' : 'Market/country/region :' + country + '.') +
     (audience === null ? '' : 'Audience : ' + audience + '.') +
     (intent === null ? '' : 'Content intent : ' + audience + '.')
   return prompt
@@ -31,21 +31,32 @@ export function getPromptForMainKeyword () {
 }
 
 export function getPromptForIntroduction () {
-  const prompt = 'Write the introduction of this blog post. Do not add a heading.Your responses should be in the markdown format in a block code.'
+  // const prompt = 'Write the introduction of this blog post. depending on the topic : explain the context and/or explain some the main problem. ' +
+  // 'If possible, give some facts. Do not describe or introduce the content of the differents headings ' +
+  // ' Do not add a heading. Your responses should be in the markdown format in a block code.'
+  const prompt =
+  'Compose a captivating introduction for this blog post topic, without using phrases such as "In this article,..." to introduce the subject.' +
+  'Instead, focus on creating a hook to capture the reader\'s attention, setting the tone and style, and seamlessly leading the reader into the main content of the article.' +
+  'Your introduction should entice readers to continue reading the article and learn more about the subject presented.' + 
+  ' Do not add a heading. Your responses should be in the markdown format in a block code.'
   return prompt
 }
 
-export function getPromptForHeading (title : string, keywords : string[] | null) : string {
+export function getPromptForHeading (title : string, keywords : string[] | null) {
   const promptAboutKeywords = keywords ? ' based on the following list of keywords : ' + keywords.join(', ') + '.' : ''
   const prompt =
-    'Write the content for the heading (without the heading)"' + title + '"' + promptAboutKeywords +
-    'Do not add a conclusion in the content or a summary of this heading. Your responses should be in the markdown format in a block code.'
+    'Write the content for the heading (without the heading)" ' + title + '"' + promptAboutKeywords +
+    'Do not add a conclusion or a summary at the end of this heading. Your responses should be in the markdown format in a block code.'
   return prompt
 }
 
-export function getPromptForConclusion () : string {
-  const prompt = 'Write the conclusion of this blog post. Do not add a heading.' +
-    'Do NOT begin the conclusion with an adverb or a preposition like "in sum","in conclusion", "in the end", "finally", "to conclude", "to sum up", "in short", "in brief", "in summary", "in esse, ...' +
-    'Your responses should be in the markdown format in a block code.'
+export function getPromptForConclusion () {
+  // const prompt = 'Write the conclusion of this blog post. Do not add a heading.' +
+  //   'Do NOT begin the conclusion with an adverb or a preposition like "in sum","in conclusion", "in the end", "finally", "to conclude", "to sum up", "in short", "in brief", "in summary", "in esse, ...' +
+  //   'Your responses should be in the markdown format in a block code.'
+  const prompt = 'Write a compelling conclusion for this blog post topic, without using transitional phrases such as "in conclusion," "in summary," or "in short".' +
+  'Focus on summarizing the main points of the post, emphasizing the significance of the topic, and leaving the reader with a lasting impression or a thought-provoking final remark.' +
+  'Ensure that your conclusion effectively wraps up the article and reinforces the central message or insights presented in the blog post.' +
+  ' Do not add a heading. Your responses should be in the markdown format in a block code.'
   return prompt
 }
