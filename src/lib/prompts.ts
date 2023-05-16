584b0309166a13606772c7f08087487e6ed6707b
Christophe <christophe.lombart@gmail.com>
2023-05-16 15:22:19 +0200

    Review the prompts

--- src/lib/prompts.ts
+++ src/lib/prompts.ts
@@ -1,6 +1,6 @@
 import { PostPrompt } from '../types'
 
-const STRUCTURE_OUTLINE = 'Generate the blog post outline with the following json format in a code block : ' +
+const STRUCTURE_OUTLINE = 'Generate the blog post outline with the following json format : ' +
 '{"title": "", // Add the post title here ' +
 '"headings" : [ { "title": "", //Add the heading title here ' +
   '"keywords": ["...", "...", "...", "..."], // Add a list of keywords here. they will help to generate the final content of this heading.' +
@@ -13,12 +13,12 @@ const STRUCTURE_OUTLINE = 'Generate the blog post outline with the following jso
 
 const INFORMATIVE_INTRO_PROMPT = 'Compose the introduction for this blog post topic, without using phrases such as "In this article,..." to introduce the subject.' +
   'Instead, explain the context and/or explain the main problem. If possible, give some facts. Do not describe or introduce the content of the differents headings of the outline' +
-  ' Do not add a heading. Your responses should be in the markdown format in a code block .'
+  ' Do not add a heading. Your responses should be in the markdown format.'
 
 const CAPTIVATING_INTO_PROMPT = 'Compose a captivating introduction for this blog post topic, without using phrases such as "In this article,..." to introduce the subject.' +
   'Instead, focus on creating a hook to capture the reader\'s attention, setting the tone and style, and seamlessly leading the reader into the main content of the article.' +
   'Your introduction should entice readers to continue reading the article and learn more about the subject presented.' +
-  ' Do not add a heading. Your responses should be in the markdown format in a code block.'
+  ' Do not add a heading. Your responses should be in the markdown format.'
 
 export function getAutoSystemPrompt (postPrompt : PostPrompt) {
   return 'You are a copywriter with a strong expertise in SEO. I need a detailed blog post in ' + postPrompt.language + ' about the topic : "' + postPrompt.topic + '".'
@@ -55,7 +55,7 @@ export function getPromptForOutline (postPrompt : PostPrompt) {
 }
 
 export function getPromptForMainKeyword () {
-  const prompt = 'Give me the most important SEO keyword in a code block with a json array in which each item match to a word without the stop words'
+  const prompt = 'Give me the most important SEO keyword in a json array in which each item match to a word without the stop words'
   return prompt
 }
 
@@ -71,7 +71,7 @@ export function getPromptForConclusion () {
   return 'Write a compelling conclusion for this blog post topic, without using transitional phrases such as "in conclusion," "in summary," "in short", "so", "thus", ... or any other transitional expression' +
   'Focus on summarizing the main points of the post, emphasizing the significance of the topic, and leaving the reader with a lasting impression or a thought-provoking final remark.' +
   'Ensure that your conclusion effectively wraps up the article and reinforces the central message or insights presented in the blog post.' +
-  'Do not add a heading. Your responses should be in the markdown format in a code block.'
+  'Do not add a heading. Your responses should be in the markdown format.'
 }
 
 export function getSeoSystemPrompt (postPrompt : PostPrompt) {
@@ -114,7 +114,7 @@ export function getPromptForSeoInfo (postPrompt : PostPrompt) {
 function getPromptForInformativeHeading (title : string, keywords : string[] | null) {
   const promptAboutKeywords = keywords ? ' based on the following list of keywords : ' + keywords.join(', ') + '.' : ''
   return 'Write an informative content for the heading (without the heading) : "' + title + '"' + promptAboutKeywords +
-    'Do not add a conclusion or a summary at the end of this heading. Your response should be in the markdown format in a code block.'
+    'Do not add a conclusion or a summary at the end of this heading. Your response should be in the markdown format.'
 }
 
 function getPromptForCaptivatingHeading (title : string, keywords : string[] | null) {
@@ -122,5 +122,5 @@ function getPromptForCaptivatingHeading (title : string, keywords : string[] | n
 
   return 'Write an captivating content for the heading (without the heading) : "' + title + '"' + promptAboutKeywords +
   'Ensure to providing in-depth information and valuable insights.Use clear and concise language, along with relevant examples or anecdotes, to engage the reader and enhance their understanding.' +
-  'Do not add a conclusion or a summary at the end of this heading. Your response should be in the markdown format in a code block.'
+  'Do not add a conclusion or a summary at the end of this heading. Your response should be in the markdown format.'
 }
